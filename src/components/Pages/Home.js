5f594cea65ef61a6478bcfc1a92fa42eadc2c4a9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-07 22:16:02 +0600

    home Product page add

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner/Banner";
+import HomeProduct from "./HomeProduct/HomeProduct";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <HomeProduct></HomeProduct>
     </div>
   );
 };
