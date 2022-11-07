a348620c8e01a1c718aad5fc16d3b9949481e737
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-07 22:11:44 +0600

    footer css add

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -5,7 +5,7 @@ import logo from "../../../assets/logo.png";
 const Footer = () => {
   return (
     <div>
-      <footer className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100">
+      <footer className="px-4 divide-y bg-[#eee] dark:text-gray-900">
         <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
           <div className="lg:w-1/3">
             <Link
@@ -21,7 +21,7 @@ const Footer = () => {
           </div>
           <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
             <div className="space-y-3">
-              <h3 className="tracking-wide uppercase dark:text-gray-50">
+              <h3 className="tracking-wide uppercase dark:text-gray-900 font-bold">
                 Product
               </h3>
               <ul className="space-y-1">
@@ -48,7 +48,7 @@ const Footer = () => {
               </ul>
             </div>
             <div className="space-y-3">
-              <h3 className="tracking-wide uppercase dark:text-gray-50">
+              <h3 className="tracking-wide uppercase dark:text-gray-900 font-bold">
                 Company
               </h3>
               <ul className="space-y-1">
@@ -65,7 +65,9 @@ const Footer = () => {
               </ul>
             </div>
             <div className="space-y-3">
-              <h3 className="uppercase dark:text-gray-50">Developers</h3>
+              <h3 className="uppercase dark:text-gray-900 font-bold">
+                Developers
+              </h3>
               <ul className="space-y-1">
                 <li>
                   <Link rel="noopener noreferrer" to="#">
@@ -85,7 +87,9 @@ const Footer = () => {
               </ul>
             </div>
             <div className="space-y-3">
-              <div className="uppercase dark:text-gray-50">Social media</div>
+              <div className="uppercase dark:text-gray-900 font-bold">
+                Social media
+              </div>
               <div className="flex justify-start space-x-3">
                 <Link
                   rel="noopener noreferrer"
