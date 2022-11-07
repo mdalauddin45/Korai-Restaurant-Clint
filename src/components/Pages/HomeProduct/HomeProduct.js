4c28613e4675d6900350f3f43c30c1a44b7b50fd
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-07 23:12:43 +0600

    products page add

--- src/components/Pages/HomeProduct/HomeProduct.js
+++ src/components/Pages/HomeProduct/HomeProduct.js
@@ -3,7 +3,7 @@ import React from "react";
 function HomeProduct() {
   return (
     <div className="bg-[#eee]">
-      <h1>this is home product</h1>
+      <button>See All</button>
     </div>
   );
 }
