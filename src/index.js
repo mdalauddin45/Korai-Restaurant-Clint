21b3337630f76f190c4aa614bf9034d15f30e139
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-08 12:55:04 +0600

    index add

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import UserContext from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ToastContainer />
-    <App />
+    <UserContext>
+      <ToastContainer />
+      <App />
+    </UserContext>
   </React.StrictMode>
 );
 
