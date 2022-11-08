7095b9e3dad1be92a17ddd9a769608894b161514
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-08 19:55:07 +0600

    home banar add

--- src/components/Pages/Banner/Banner.js
+++ src/components/Pages/Banner/Banner.js
@@ -1,59 +1,31 @@
 import React from "react";
-import img1 from "../../../assets/London-Slide-Cover.jpg";
-// import img1 from "../../../assets/popular1.png";
-import img2 from "../../../assets/popular2.png";
-import img3 from "../../../assets/popular4.png";
-import img4 from "../../../assets/trending1.png";
-import img5 from "../../../assets/trending2.png";
-import img6 from "../../../assets/trending3.png";
-import BannerData from "./BannerData";
-import "./BannerItem.css";
-
-const bannerData = [
-  {
-    image: img1,
-    prev: 6,
-    id: 1,
-    next: 2,
-  },
-  {
-    image: img2,
-    prev: 1,
-    id: 2,
-    next: 3,
-  },
-  {
-    image: img3,
-    prev: 2,
-    id: 3,
-    next: 4,
-  },
-  {
-    image: img4,
-    prev: 3,
-    id: 4,
-    next: 5,
-  },
-  {
-    image: img5,
-    prev: 4,
-    id: 5,
-    next: 6,
-  },
-  {
-    image: img6,
-    prev: 5,
-    id: 6,
-    next: 1,
-  },
-];
-
+import { Link } from "react-router-dom";
+import img from "../../../assets/Products image/slider-courier-mask.png";
 const Banner = () => {
   return (
-    <div className="carousel w-full py-10">
-      {bannerData.map((slide) => (
-        <BannerData key={slide.id} slide={slide}></BannerData>
-      ))}
+    <div className="grid lg:grid-cols-2">
+      <div className="text-black  pt-20 px-36">
+        <h1 className="text-5xl font-extrabold pb-5">
+          <span className="text-yellow-400">Korai</span> <br />
+          <span className="text-green-500"> Home Delivery</span>
+        </h1>
+        <p className="text-xl pb-10">
+          Curabitur imperdiet varius lacus, id placerat purus vulputate non.
+          Fusce in felis vel arcu maximus placerat eu ut arcu. Ut nunc ex,
+          gravida vel porttitor et, pretium ac sapien.
+        </p>
+        <Link to="/product">
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold rounded-full dark:bg-green-500 dark:text-white mb-4"
+          >
+            Order Now
+          </button>
+        </Link>
+      </div>
+      <div>
+        <img src={img} alt="/" />
+      </div>
     </div>
   );
 };
