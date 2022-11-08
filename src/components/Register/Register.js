4637ad7c9ac4b5729a43477c4724b1f9993a3fcb
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-08 13:19:33 +0600

    register clrr

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,82 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import { useContext } from "react";
+import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { AuthContext } from "../../contexts/UserContext";
 
 const Register = () => {
+  const [error, setError] = useState(" ");
+  const { signInWithGoogle, gitHunSignIn, createUser, updateName } =
+    useContext(AuthContext);
+  // navigate
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  // sign up email password
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        setError("");
+
+        // Update Name
+        updateName(name)
+          .then(() => {
+            toast.success("your name updated");
+            navigate(from, { replace: true });
+          })
+          .catch((error) => console.error(error));
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
+  };
+
+  // icon click to login part
+  // 1. Google Sign in with pop up
+  const handleGoogleSignin = () => {
+    signInWithGoogle()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast.success("Sign up Succesfuly");
+        navigate(from, { replace: true });
+      })
+
+      .catch((error) => {
+        console.error(error);
+        toast.error("Sign up not Succesfuly!! Pleace try again");
+      });
+  };
+
+  // 2.Github sign in with pop up
+
+  const handleGithubSignIn = () => {
+    gitHunSignIn()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast.success("Sign up Succesfuly");
+        navigate(from, { replace: true });
+      })
+
+      .catch((error) => {
+        console.error(error);
+        toast.error("Sign up not Succesfuly!! Pleace try again");
+      });
+  };
   return (
     <div className="max-w-screen-xl mx-auto p-10">
       <div className="w-fullmax-w-screen-xl mx-auto max-w-md p-8 space-y-3 rounded-xl bg-white dark:text-gray-900 shadow-xl">
@@ -11,8 +86,7 @@ const Register = () => {
           are visiting
         </p>
         <form
-          novalidate=""
-          action=""
+          onSubmit={handleSubmit}
           className="space-y-6 ng-untouched ng-pristine ng-valid"
         >
           <div className="space-y-1 text-sm">
@@ -36,9 +110,7 @@ const Register = () => {
             />
           </div>
           <div className="space-y-1 text-sm">
-            <label for="password" className="block dark:text-gray-400">
-              Password
-            </label>
+            <label className="block dark:text-gray-400">Password</label>
             <input
               type="password"
               name="password"
@@ -46,6 +118,9 @@ const Register = () => {
               placeholder="Password"
               className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-100 dark:text-gray-900 focus:dark:border-green-400"
             />
+          </div>
+          <div>
+            <p>{error}</p>
           </div>
           <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-green-400">
             Sign in
@@ -59,7 +134,11 @@ const Register = () => {
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
         <div className="flex justify-center space-x-4">
-          <button aria-label="Log in with Google" className="p-3 rounded-sm">
+          <button
+            onClick={handleGoogleSignin}
+            aria-label="Log in with Google"
+            className="p-3 rounded-sm"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 32 32"
@@ -68,16 +147,11 @@ const Register = () => {
               <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
             </svg>
           </button>
-          <button aria-label="Log in with Twitter" className="p-3 rounded-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-              className="w-5 h-5 fill-current"
-            >
-              <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
-            </svg>
-          </button>
-          <button aria-label="Log in with GitHub" className="p-3 rounded-sm">
+          <button
+            onClick={handleGithubSignIn}
+            aria-label="Log in with GitHub"
+            className="p-3 rounded-sm"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 32 32"
