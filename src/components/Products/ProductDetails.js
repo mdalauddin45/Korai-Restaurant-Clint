1f75c32eb6de8eaef6349d6bc3d47a3124a398e3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-08 15:39:03 +0600

    review fild add

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faStarHalfAlt } from "@fortawesome/free-solid-svg-icons";
 
 const ProductDetails = () => {
   const products = useLoaderData();
   const { image, name, price, rating, description, _id } = products.data;
   //   console.log(products);
+  const handleReview = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const message = form.message.value;
+    console.log(message);
+  };
   return (
     <div className="px-10 py-20">
       <div className=" p-2 max-w-screen-xl mx-auto  shadow-xl  bg-gray-50 text-gray-900">
@@ -15,16 +23,21 @@ const ProductDetails = () => {
               alt=""
               className="block object-cover object-center w-full rounded-md h-96 dark:bg-gray-500"
             />
-            <div className="flex items-center text-xs">
-              <span>6 min ago</span>
-            </div>
           </div>
           <div className="space-y-2">
-            <p rel="noopener noreferrer" className="block">
-              <h3 className="text-xl font-semibold dark:text-green-400">
+            <h1 rel="noopener noreferrer" className="block">
+              <p className="text-2xl font-semibold dark:text-green-400">
                 {name}
-              </h3>
-            </p>
+              </p>
+              <p className="text-black text-xl font-bold"> Price: ${price}</p>
+            </h1>
+            <h1>Rating: {rating}</h1>
+            <h1 className="text-yellow-400 text-xl">
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStarHalfAlt} />
+            </h1>
             <p className="leading-snug dark:text-gray-900">{description}</p>
           </div>
           <div className="flex flex-wrap justify-between">
@@ -87,6 +100,82 @@ const ProductDetails = () => {
                 </svg>
                 <span>283</span>
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pt-10">
+        <h1 className="text-center text-2xl text-gray-600 font-bold">
+          COUSTOMER REVIEW
+        </h1>
+      </div>
+      <div className="pt-5">
+        <div className=" p-2 max-w-screen-xl mx-auto  shadow-xl  bg-gray-50 text-gray-900 ">
+          <div className="flex justify-between p-4">
+            <div className="flex space-x-4">
+              <div>
+                <img
+                  src="https://source.unsplash.com/100x100/?portrait"
+                  alt=""
+                  className="object-cover w-12 h-12 rounded-full bg-gray-500"
+                />
+              </div>
+              <div>
+                <h4 className="font-bold">Leroy Jenkins</h4>
+                <span className="text-xs text-gray-400">2 days ago</span>
+              </div>
+            </div>
+            <div className="flex items-center space-x-2 text-yellow-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                className="w-5 h-5 fill-current"
+              >
+                <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+              </svg>
+              <span className="text-xl font-bold">4.5</span>
+            </div>
+          </div>
+          <div className="p-4 space-y-2 text-sm text-gray-400">
+            <p>
+              Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu
+              dictum lectus consequat vitae. Etiam ut dolor id justo fringilla
+              finibus. Donec eget ultricies diam, eu molestie arcu. Etiam nec
+              lacus eu mauris cursus venenatis. Maecenas gravida urna vitae
+              accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum
+              sem.
+            </p>
+          </div>
+        </div>
+        <div className="pt-5">
+          <div className=" p-2 max-w-screen-xl mx-auto  shadow-xl  bg-gray-50 text-gray-900">
+            <div className="flex flex-col items-center w-full">
+              <h2 className="text-3xl font-semibold text-center">
+                Your opinion matters!
+              </h2>
+              <div className="flex flex-col items-center py-6 space-y-3">
+                <span className="text-center">How was your experience?</span>
+                <div className="flex space-x-3 text-3xl text-yellow-500">
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStarHalfAlt} />
+                </div>
+              </div>
+              <form onSubmit={handleReview} className="flex flex-col w-full">
+                <input
+                  type="text"
+                  name="message"
+                  placeholder="Message..."
+                  className="p-4 rounded-md resize-none dark:text-gray-900 dark:bg-gray-200"
+                ></input>
+                <button
+                  type="submit"
+                  className="py-4 my-8 font-semibold rounded-md dark:text-gray-900 dark:bg-green-400"
+                >
+                  Send feedback
+                </button>
+              </form>
             </div>
           </div>
         </div>
