edbec6fb5f4129e762e48824865cf3674fbcfb1a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-09 23:44:53 +0600

    update review page style add

--- src/components/Review/UpdateReview.js
+++ src/components/Review/UpdateReview.js
@@ -1,3 +1,6 @@
+import { faArrowsRotate, faTruckFast } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessages } from "@fortawesome/sharp-solid-svg-icons";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -84,8 +87,34 @@ const UpdateReview = () => {
           </fieldset>
         </form>
       </section>
-      <div>
-        <h1>hi</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 max-w-screen-xl mx-auto pt-20 pb-20">
+        <div className="text-white flex bg-green-400 rounded pt-10 pb-10 shadow-xl m-8">
+          <p>
+            <FontAwesomeIcon className="text-4xl p-4" icon={faTruckFast} />
+          </p>
+          <div>
+            <h1 className="text-4xl font-bold">Free shipping</h1>
+            <p className="text-xl">Free Shipping on orders $199.</p>
+          </div>
+        </div>
+        <div className="text-white flex  bg-yellow-400 rounded pt-10 pb-10 shadow-xl m-8">
+          <p>
+            <FontAwesomeIcon className="text-4xl p-4" icon={faMessages} />
+          </p>
+          <div>
+            <h1 className="text-4xl font-bold">24/7 Support</h1>
+            <p className="text-xl">Online and phone support 24 / 7</p>
+          </div>
+        </div>
+        <div className="text-white flex  bg-red-400 rounded pt-10 pb-10 shadow-xl m-8">
+          <p>
+            <FontAwesomeIcon className="text-4xl p-4" icon={faArrowsRotate} />
+          </p>
+          <div>
+            <h1 className="text-4xl font-bold">24 Hours Return</h1>
+            <p className="text-xl">24 hours money-back guarantee.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
